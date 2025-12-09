a2d239d12e37a25114a3d0f8847a19190d34206f
TAMURASOUSUKE <kokotomo.88934@gmail.com>
2025-12-09 12:01:17 +0900

    各ゲームシーンのマネージャースクリプトを追加[WIP]
    [WIP] = Work In Progressなのでまだ作業中ということ

---
+++ Contents_02/Assets/Scripts/GameLoop/GameSceneManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+
+/*
+    ゲームシーンの切り替え等を行う
+*/
+public class GameSceneManager : MonoBehaviour
+{
+    void Start()
+    {
+        Application.targetFrameRate = 60; // FPSを60に固定
+    }
+
+    void Update()
+    {
+        
+    }
+
+
+
+}
